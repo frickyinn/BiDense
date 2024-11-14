d3efa6722e640e53bb9367bf4c78231dd3bc16b4
殷睿 <7997418+yin-rui-2000@user.noreply.gitee.com>
2024-11-14 14:25:29 +0800

    first commit

--- models/upernet/__init__.py
+++ models/upernet/__init__.py
@@ -1,9 +1,8 @@
-from .upernet import UperNetDepthModel, UperNetSegmentationModel
-from .fp32_upernet import FP32UperNetDepthModel, FP32UperNetSegmentationModel
-from .bnn_upernet import BnnUperNetDepthModel, BnnUperNetSegmentationModel
-from .react_upernet import ReActUperNetDepthModel, ReActUperNetSegmentationModel
-from .adabin_upernet import AdaBinUperNetDepthModel, AdaBinUperNetSegmentationModel
-from .bisrnet_upernet import BiSRUperNetDepthModel, BiSRUperNetSegmentationModel
-from .cfb_upernet import CFBUperNetDepthModel, CFBUperNetSegmentationModel
+# from .upernet import UperNetDepthModel, UperNetSegmentationModel
+# from .fp32_upernet import FP32UperNetDepthModel, FP32UperNetSegmentationModel
+# from .bnn_upernet import BnnUperNetDepthModel, BnnUperNetSegmentationModel
+# from .react_upernet import ReActUperNetDepthModel, ReActUperNetSegmentationModel
+# from .adabin_upernet import AdaBinUperNetDepthModel, AdaBinUperNetSegmentationModel
+# from .bisrnet_upernet import BiSRUperNetDepthModel, BiSRUperNetSegmentationModel
+# from .cfb_upernet import CFBUperNetDepthModel, CFBUperNetSegmentationModel
 from .bidense_upernet import BiDenseUperNetDepthModel, BiDenseUperNetSegmentationModel
-from .test_upernet import TestUperNetDepthModel, TestUperNetSegmentationModel
